98babee240fb42d8bff5ad869dbb308d1b3e6d2a
Simone-Fratini <simone@fratini.com>
2025-01-13 17:30:00 +0100

    Remove unused imports in CardPage component

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import MainContext  from '../contexts/MainContext';
 
