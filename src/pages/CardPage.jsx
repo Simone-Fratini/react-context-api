4614026b73d1771fba8e75119bb0e346b67229f1
Simone-Fratini <simone@fratini.com>
2025-01-13 17:27:06 +0100

    Refactor CardPage to use MainContext for post data instead of axios

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -1,34 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import MainContext  from '../contexts/MainContext';
 
 function CardPage() {
   const { id } = useParams(); 
-  const [cardData, setCardData] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3000/posts/${id}`) 
-      .then((response) => setCardData(response.data))
-      .catch((error) => console.error('Errore nel caricamento dei dati:', error));
-  }, [id]);
-
-  // Mostra un messaggio di caricamento
-  if (!cardData) {
-    return <div className="text-white text-center mt-20">Loading...</div>;
-  }
+  const { posts } = useContext(MainContext)
+  const trueId = id - 1
 
   return (
     <div className="min-h-screen bg-[#150050] text-white flex items-center justify-center p-6">
       <div className="bg-[#000000] p-8 rounded-lg shadow-lg max-w-lg">
         <img
-          src={cardData.img}
-          alt={cardData.name}
+          src={posts[trueId].img}
+          alt={posts[trueId].name}
           className="w-full h-64 object-cover rounded-lg mb-4"
         />
-        <h1 className="text-3xl font-bold mb-4">{cardData.name}</h1>
-        <p className="mb-4">{cardData.content}</p>
-        <div className="text-lg font-semibold mb-4">Price: €{cardData.price}</div>
+        <h1 className="text-3xl font-bold mb-4">{posts[trueId].name}</h1>
+        <p className="mb-4">{posts[trueId].content}</p>
+        <div className="text-lg font-semibold mb-4">Price: €{posts[trueId].price}</div>
       </div>
     </div>
   );
